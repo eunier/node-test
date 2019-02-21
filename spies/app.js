bff177bb57340d2d5d8a2df09bbb30488ce31a6c
Yunier Alvarez <yunieralvarezdev@icloud.com>
2019-02-20 20:43:25 -0500

    Remove unnecessary comment

--- spies/app.js
+++ spies/app.js
@@ -3,7 +3,4 @@ var db = require('./db.js');
 module.exports.handleSignup = (email, password)=> {
   // check if email already exits
   db.saveUser({email, password});
-  
-  // vase teh user to the database
-  // send the welcome email
 };
