8fa4da08f24cbf29e8b950a346f18ce739db5a55
Yunier Alvarez <yunieralvarezdev@icloud.com>
2019-02-20 20:35:25 -0500

    Add more spy tests

--- server/app.test.js
+++ server/app.test.js
@@ -1,9 +1,25 @@
 const expect = require('expect');
+const rewire = require('rewire');
+
+var app = rewire('./app');
 
 describe('App', () => {
+  var db = {
+    saveUser: expect.createSpy()
+  };
+  app.__set__('db', db);
+
   it('should call the spy correctly', () => {
     var spy = expect.createSpy();
     spy('Yuier', 24);
     expect(spy).toHaveBeenCalledWith('Yuier', 24);
+  });
+
+  it('should call saveUser with user object', () => {
+    var email = 'name@mail.com';
+    var password = '123abc';
+
+    app.handleSignup(email, password);
+    expect(db.saveUser).toHaveBeenCalledWith({email, password});
   });
 });
